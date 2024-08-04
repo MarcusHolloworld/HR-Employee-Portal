22388bfae879219e57a689f488c8924d43e821eb
Rohit Bhowmik <rohitbhowmik1909@gmail.com>
2024-08-04 21:43:16 +0530

    First Commmit

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+	//public List<Employee> findAllByOrderByLastNameAsc();
+
+	public List<Employee> findAllByOrderByFirstNameAsc();
+
+}
